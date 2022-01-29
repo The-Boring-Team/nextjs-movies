8c050198f51fabb140659a8849680cd2c0b7209c
Eclipse <tepes_alexandru@outlook.com>
2022-01-29 22:06:31 +0200

    external script

--- pages/movie.tsx
+++ pages/movie.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Movie() {
   return (
@@ -9,6 +10,11 @@ export default function Movie() {
         <title>Iron Man</title>
         <link rel="icon" href="/images/profile.png" />
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`script loaded successfully.`)}
+      />
       My first movie page!
       <Link href="/">
         <a>Homepage</a>
