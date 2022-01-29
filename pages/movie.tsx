7a36ec695ccc803f351218766e9452e8baec057f
Eclipse <tepes_alexandru@outlook.com>
2022-01-29 21:35:07 +0200

    title and favicon

--- pages/movie.tsx
+++ pages/movie.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Movie() {
   return (
     <div>
+      <Head>
+        <title>Iron Man</title>
+        <link rel="icon" href="/images/profile.png" />
+      </Head>
       My first movie page!
       <Link href="/">
         <a>Homepage</a>
