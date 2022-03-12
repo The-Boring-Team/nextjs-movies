95defca6ef69de38fd8eedd9f4ca1297e0e4b370
Eclipse <tepes_alexandru@outlook.com>
2022-03-12 12:29:14 +0200

    extended movieprops

--- types.ts
+++ types.ts
@@ -1,6 +1,15 @@
+export type Genre = {
+  id: number;
+  name: string;
+};
+
 export type MovieProps = {
   id: number;
   title: string;
   release_date: Date;
   poster_path: string;
+  backdrop_path: string;
+  overview: string;
+  genres: Genre[];
+  runtime: number;
 };
